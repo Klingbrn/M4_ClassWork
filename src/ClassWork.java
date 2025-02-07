c2ca08dd680abe32294214bf65b84662eef482fd
klingbrn <klingbrn@mail.uc.edu>
2025-02-07 11:03:42 -0500

    My first Java classwork

--- src/ClassWork.java
+++ src/ClassWork.java
@@ -1,5 +1,21 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+public class ClassWork {
+    public static void main(String[] args)
+    {
+        int noPersons = 4;
+        double dishPrice = 18.99;
+        final double TAX_RATE = 0.2;
+        double subTotal = noPersons * dishPrice;
+        double tax = subTotal * TAX_RATE;
+        double tip = subTotal * 0.15;
+        double total = subTotal + tax + tip;
+        double share_person = 0;
+
+        System.out.println("total Subtotal: " + subTotal);
+        System.out.println("Total tax " + tax);
+        System.out.println("tip: " + tip);
+        System.out.println("Total: " + total);
+
+        share_person = total/4;
+        System.out.println("share per person; " + share_person);
     }
 }
